3ba8eb7a6ee9c0ac0949b8a133fb501fb225d3e3
szulcu <szulcu@1582fed9-3eea-4c7b-b5d3-d16645abbf50>
2014-07-02 15:34:40 +0000

    Przygotowywanie tłumaczenia MyBB 1.8 beta 2

---
+++ beta2/inc/languages/polish/contact.lang.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * MyBB 1.8 Polish Language Pack
+ * Copyright © 2008-2011 MyBBoard.pl Team
+ * See readme.html for copyright information. | Zapoznaj sie z plikiem czytaj_to.html.
+ * Autorzy: bryn, DamYan, gigi, matc, szulcu | Testerzy: hippie
+ */
+
+$l['contact'] = 'Kontakt';
+$l['nav_contact'] = 'Kontakt';
+$l['contact_no_message'] = 'Nie wpisano treści wiadomości.';
+$l['contact_no_subject'] = 'Nie wpisano tematu wiadomości.';
+$l['contact_no_email'] = 'Nie wpisano poprawnego adresu e-mail.';
+$l['contact_success_message'] = 'Wiadomość została wysłana do administratora.';
+$l['contact_subject'] = 'Temat';
+$l['contact_message'] = 'Treść';
+$l['contact_email'] = 'Adres e-mail';
+$l['contact_email_desc'] = 'Wpisz adres e-mail, aby administrator mógł odpowiedzieć na twoją wiadomość.';
+$l['contact_send'] = 'Wyślij';
+$l['image_verification'] = "Potwierdzenie";
+$l['verification_note'] = "Przepisz tekst z obrazka do poniższego pola tekstowego. Taki proces jest niezbędny, by zapobiec wysyłaniu wiadomości przez automaty.";
+$l['verification_subnote'] = "(wielkość liter nie ma znaczenia)";
+$l['invalid_captcha'] = "Wprowadzony kod z obrazka jest nieprawidłowy. Przepisz kod identyczny jak ten na obrazku.";
+?>
---
+++ beta2/inc/languages/polish/polls.lang.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * MyBB 1.8 Polish Language Pack
+ * Copyright © 2008-2014 MyBBoard.pl Team
+ * See readme.html for copyright information. | Zapoznaj sie z plikiem czytaj_to.html.
+ * Autorzy: bryn, DamYan, gigi, matc, szulcu | Testerzy: hippie
+ */
+
+$l['nav_postpoll'] = "Utwórz ankietê";
+$l['nav_editpoll'] = "Edytuj ankietê";
+$l['nav_pollresults'] = "Wyniki ankiety";
+
+$l['edit_poll'] = "Edytuj ankietê";
+$l['delete_poll'] = "Usuñ ankietê";
+$l['delete_q'] = "Usun¹æ?";
+$l['delete_note'] = "Aby usun¹æ tê ankietê, zaznacz pole po lewej i kliknij w przycisk \"Usuñ ankietê\".";
+$l['delete_note2'] = "<b>Uwaga:</b> Po usuniêciu ankiety nie bêdzie ju¿ mo¿liwoœci jej przywrócenia.";
+$l['question'] = "Pytanie:";
+$l['num_options'] = "Liczba odpowiedzi:";
+$l['max_options'] = "Maksymalnie";
+$l['poll_options'] = "Odpowiedzi ankiety:";
+$l['update_options'] = "Aktualizuj liczbê odpowiedzi";
+$l['poll_options_note'] = "Odpowiedzi ankiety powinny byæ krótkie i zwiêz³e.";
+$l['options'] = "Opcje:";
+$l['option_multiple'] = "<b>Ankieta wielokrotnego wyboru:</b> u¿ytkownik bêdzie móg³ wybraæ wiele odpowiedzi.";
+$l['option_public'] = "<b>Ankieta publiczna:</b> u¿ytkownicy bêd¹ mogli wzajemnie poznaæ swoje odpowiedzi.";
+$l['option_closed'] = "<b>Ankieta zamkniêta:</b> u¿ytkownicy nie bêd¹ mogli odpowiadaæ.";
+$l['poll_timeout'] = "Data wygaœniêcia:";
+$l['timeout_note'] = "Liczba dni do zakoñczenia g³osowania. <br />Jeœli wpiszesz 0, ankieta nigdy nie wygaœnie.";
+$l['days_after'] = "dni od";
+$l['update_poll'] = "Aktualizuj ankietê";
+$l['option'] = "OdpowiedŸ";
+$l['votes'] = "G³osów:";
+$l['post_new_poll'] = "Zapisz ankietê";
+$l['days'] = "dni";
+$l['poll_results'] = "Wyniki ankiety";
+$l['poll_total'] = "Razem:";
+$l['poll_votes'] = "g³osów";
+
+$l['redirect_pollposted'] = "Twoja ankieta zosta³a utworzona.<br />Teraz nast¹pi przeniesienie do w¹tku.";
+$l['redirect_pollpostedmoderated'] = "Twoja ankieta zosta³a utworzona, jednak przed opublikowaniem musi byæ zweryfikowana przez moderatora.<br />Teraz nast¹pi przeniesienie na forum.";
+$l['redirect_pollupdated'] = "Ankieta zosta³a zaktualizowana.<br />Teraz nast¹pi przeniesienie do w¹tku.";
+$l['redirect_votethanks'] = "Dziêkujemy za wype³nienie ankiety.<br />Teraz nast¹pi przeniesienie do w¹tku.";
+$l['redirect_unvoted'] = "Twój g³os w ankiecie zosta³ usuniêty.<br />Teraz nast¹pi przeniesienie do w¹tku.";
+$l['redirect_polldeleted'] = "Ankieta zosta³a usuniêta z w¹tku.<br />Teraz nast¹pi przeniesienie do w¹tku.";
+
+$l['error_polloptiontoolong'] = "Jedna lub wiêcej odpowiedzi jest za d³uga. Wróæ i skróæ je.";
+$l['error_noquestionoptions'] = "Musisz wprowadziæ pytanie do ankiety lub wiêcej odpowiedzi. Minimalna liczba odpowiedzi to 2.<br />Wróæ i popraw ten b³¹d.";
+$l['error_pollalready'] = "Ten w¹tek ma ju¿ ankietê!";
+$l['error_nopolloptions'] = "Wybrana odpowiedŸ ankiety jest nieprawid³owa lub nie istnieje.";
+$l['error_alreadyvoted'] = "Wziêto ju¿ wczeœniej udzia³ w tej ankiecie.";
+$l['error_notvoted'] = "W tej ankiecie nie brano udzia³u.";
+$l['error_invalidpoll'] = "Wybrana ankieta nie istnieje lub jest nieprawid³owa.";
+$l['error_pollclosed'] = "Ta ankieta jest ju¿ zakoñczona.";
+$l['poll_time_limit'] = "Nie mo¿esz dodaæ ankiety do swojego w¹tku. Administrator na³o¿y³ limit czasowy na dodawanie ankiet i mo¿na je dodawaæ tylko w ci¹gu pierwszych {1} godzin od zamieszczenia w¹tku.";
+
+$l['poll_deleted'] = "Usuniêto ankietê";
+$l['poll_edited'] = "Wyedytowano ankietê";
+?>
